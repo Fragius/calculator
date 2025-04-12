7f0171462b4c132fb17245ab85d2ae35cd73b3ae
fragius <189782341+Fragius@users.noreply.github.com>
2025-04-12 13:52:20 +0300

    Make function to evaluate expressions

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const TEXT_TO_FUNCTION = {
     "+": add,
     "-": subtract,
     "/": divide,
-    "*": multiply
+    "x": multiply
 }
 
 function add(num1, num2) {
@@ -23,5 +23,49 @@ function divide(num1, num2) {
 
 function operate(equation) {
     operation = TEXT_TO_FUNCTION[equation[1]];
-    return operation(equation[0], equation[2]);
+    return operation(+equation[0], +equation[2]);
 }
+
+function turnStrExpressionToArr(equation) {
+    let expressionArr = equation.split("");
+    for (let i = 1; i < expressionArr.length; i++) {
+        currItem = expressionArr[i];
+        prevItem = expressionArr[i - 1];
+        if (currItem.match(/[0-9]/) && prevItem.match(/[0-9]/)) { //check if both items are numbers
+            expressionArr[i - 1] = prevItem + currItem;
+            expressionArr.splice(i, 1)
+        }
+    }
+    if (expressionArr[0] === "-") {
+        expressionArr[0] = expressionArr[0] + expressionArr[1];
+        expressionArr.splice(1, 1);
+    }
+    return expressionArr;
+}
+
+function evaluateInOrder(expression) { //: division is multiplication 
+    for (let c = 0; c < expression.length; c++) {
+    const index = expression.findIndex(item => item.toString().match(/x|\//));
+        if (index === -1) {
+            break;
+        } else {
+            const smallExp = expression.splice(index -1, 3);
+            const result = operate(smallExp)
+            expression.splice(index - 1, 0, result);
+        }
+    }
+    while (expression.length > 1) {
+        const currentExpression = expression.splice(0, 3);
+        const solution = operate(currentExpression)
+        expression.unshift(solution);
+    };
+    return expression;
+}
+
+
+function evaluate(expression) {
+    const expressionArr = turnStrExpressionToArr(expression);
+    return evaluateInOrder(expressionArr)[0] //Evaluate in the order of division, and multiplication then addition, and subtraction
+}
+
+console.log(evaluate("1+2/4x2"));
